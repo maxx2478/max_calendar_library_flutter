43c3e08c44ef9e674245f0209fcf7300e01d0362
maxx2478 <mkpatil24@gmail.com>
2023-06-22 13:18:06 +0530

    bug fixes

--- README.md
+++ README.md
@@ -1,15 +1,4 @@
-<!-- 
-This README describes the package. If you publish this package to pub.dev,
-this README's contents appear on the landing page for your package.
-
-For information about how to write a good package README, see the guide for
-[writing package pages](https://dart.dev/guides/libraries/writing-package-pages). 
-
-For general information about developing packages, see the Dart guide for
-[creating packages](https://dart.dev/guides/libraries/create-library-packages)
-and the Flutter guide for
-[developing packages and plugins](https://flutter.dev/developing-packages). 
--->
+A beautiful calendar library for cross platforms.
 
 ## Maxx Calendar
  This package provides two month view modes.
@@ -25,6 +14,10 @@ and the Flutter guide for
 ## Getting started
 
 Just add the widget to your widget tree and go ahead !
+
+
+## Usage
+
 ```
 MaxxCalendar(
             events: listOfEvents, //List<List<MaxxEventModel>>
@@ -45,17 +38,5 @@ MaxxCalendar(
           )
 ```
 
-## Usage
 
-TODO: Include short and useful examples for package users. Add longer examples
-to `/example` folder. 
 
-```dart
-const like = 'sample';
-```
-
-## Additional information
-
-TODO: Tell users more about the package: where to find more information, how to 
-contribute to the package, how to file issues, what response they can expect 
-from the package authors, and more.
--- lib/maxx_calendar.dart
+++ lib/maxx_calendar.dart
@@ -1,7 +1,310 @@
 library maxx_calendar;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+
+import 'package:intl/intl.dart';
+import 'package:maxx_calendar/widget/cal_model.dart';
+import 'package:maxx_calendar/widget/calendar_dot_tile.dart';
+import 'package:maxx_calendar/widget/calendar_tile.dart';
+import 'package:maxx_calendar/widget/caltype.dart';
+import 'package:maxx_calendar/widget/constants.dart';
+import 'package:maxx_calendar/widget/views.dart';
+
+
+
+class MaxxCalendar extends StatefulWidget {
+  DateTime now = DateTime.now();
+  DateFormat formatter = DateFormat('EEE');
+  DateFormat defaultFormat = DateFormat("dd-MM-yyyy");
+  var calType = CalType.MONTHVIEW;
+  Color tileBorderColor = Colors.black54;
+  Color tileDateColor = Colors.black54;
+  var currMonth = DateTime.now().month;
+  var currYear = DateTime.now().year;
+  var firstDayOfMonth = "mon";
+  var firstDaySpacing = 0;
+  var endDaySpacing = 0;
+  var emptyEvents = <List<MaxxEventModel>>[];
+  var hideMonthNavigationButtons = false;
+
+  var events = <List<MaxxEventModel>>[];
+
+  var resultEvents = <List<MaxxEventModel>>[];
+  Function(Map<String, List<MaxxEventModel>>) onDateClick;
+  Function(String) onDateChangeListener;
+
+  MaxxCalendar(
+      {Key? key,
+        required this.calType,
+        required this.tileBorderColor,
+        required this.tileDateColor,
+        required this.events,
+        required this.onDateClick,
+        required this.onDateChangeListener,
+        required this.currMonth,
+        required this.currYear,
+        required this.hideMonthNavigationButtons})
+      : super(key: key);
+
+  @override
+  State<MaxxCalendar> createState() => _MaxxCalendarState();
+}
+
+class _MaxxCalendarState extends State<MaxxCalendar> {
+  static int getDaysInMonth(int year, int month) {
+    if (month == DateTime.february) {
+      final bool isLeapYear =
+          (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0);
+      return isLeapYear ? 29 : 28;
+    }
+    const List<int> daysInMonth = <int>[
+      31,
+      -1,
+      31,
+      30,
+      31,
+      30,
+      31,
+      31,
+      30,
+      31,
+      30,
+      31
+    ];
+    return daysInMonth[month - 1];
+  }
+
+  void createFinalList() {
+    widget.firstDayOfMonth =
+        widget.formatter.format(DateTime(widget.currYear, widget.currMonth, 0));
+
+    widget.firstDaySpacing = Constants.firstDayOffset(
+        widget.currYear,
+        (widget.currMonth),
+        const DefaultMaterialLocalizations());
+
+    if (widget.firstDaySpacing > 0) {
+      for (var i = 1; i <= widget.firstDaySpacing; i++) {
+        widget.emptyEvents.add([]);
+      }
+    }
+
+    widget.resultEvents.addAll(widget.emptyEvents);
+
+    if (widget.events.length <=
+        getDaysInMonth(widget.currYear, widget.currMonth)) {
+      widget.resultEvents.addAll(widget.events);
+    } else {
+      throw ("Events size : ${widget.events.length} - Events are more than the number of days present in current month");
+    }
+
+    var tempEndspacing = (getDaysInMonth(widget.currYear, widget.currMonth) +
+        widget.firstDaySpacing);
+    widget.endDaySpacing = 0;
+    if ((tempEndspacing) % 7 != 0) {
+      for (var i = 0; i < 10; i++) {
+        widget.endDaySpacing++;
+        if ((tempEndspacing + widget.endDaySpacing) % 7 == 0) {
+          break;
+        }
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    createFinalList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    void onNextBtnClick() {
+      setState(() {
+        widget.resultEvents = [];
+        widget.emptyEvents = [];
+        widget.currMonth != 12
+            ? {widget.currMonth = (widget.currMonth + 1)}
+            : {widget.currMonth = 1, widget.currYear = widget.currYear + 1};
+
+        createFinalList();
+      });
+      widget.onDateChangeListener("${widget.currMonth}-${widget.currYear}");
+    }
+
+    void onPrevBtnClick() {
+      setState(() {
+        widget.resultEvents = [];
+        widget.emptyEvents = [];
+        widget.currMonth != 1
+            ? {(widget.currMonth = widget.currMonth - 1)}
+            : {widget.currMonth = 12, widget.currYear = widget.currYear - 1};
+        createFinalList();
+      });
+      widget.onDateChangeListener("${widget.currMonth}-${widget.currYear}");
+    }
+
+    void invokeOnclick(Map<String, List<MaxxEventModel>> events) {
+      widget.onDateClick(events);
+    }
+
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+          child: widget.hideMonthNavigationButtons == true
+              ? const SizedBox()
+              : Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              InkWell(
+                radius: 15,
+                borderRadius: BorderRadius.circular(15),
+                onTap: () {
+                  onPrevBtnClick();
+                },
+                child: const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Icon(
+                    Icons.arrow_back_ios_new,
+                    size: 15,
+                  ),
+                ),
+              ),
+              paragraphText(
+                  "${Constants.getCalendarMonth(widget.currMonth).toString()} ${widget.currYear}",
+                  size: 12),
+              InkWell(
+                borderRadius: BorderRadius.circular(15),
+                child: const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Icon(
+                    Icons.arrow_forward_ios,
+                    size: 15,
+                  ),
+                ),
+                onTap: () {
+                  onNextBtnClick();
+                },
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 16,
+          child: GridView.count(
+            crossAxisCount: 7,
+            children: [
+              paragraphText("Mon", alignment: TextAlign.center),
+              paragraphText("Tue", alignment: TextAlign.center),
+              paragraphText("Wed", alignment: TextAlign.center),
+              paragraphText("Thu", alignment: TextAlign.center),
+              paragraphText("Fri", alignment: TextAlign.center),
+              paragraphText("Sat", alignment: TextAlign.center),
+              paragraphText("Sun", alignment: TextAlign.center),
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 9,
+          child: Animate(
+            effects: const [FadeEffect()],
+            child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    childAspectRatio:
+                    widget.calType == CalType.MONTHVIEW ? 8 / 16 : 1 / 1,
+                    crossAxisSpacing: 0,
+                    mainAxisSpacing: 0,
+                    mainAxisExtent:
+                    widget.calType == CalType.MONTHVIEW ? 84 : 64,
+                    crossAxisCount: 7),
+                itemCount: getDaysInMonth(widget.currYear, widget.currMonth) +
+                    widget.firstDaySpacing +
+                    widget.endDaySpacing,
+                itemBuilder: (BuildContext ctx, index) {
+                  var item = ((widget.resultEvents.length) - 1) >= index
+                      ? widget.resultEvents[index]
+                      : <MaxxEventModel>[];
+                  return Animate(
+                    effects: [FadeEffect(delay: 500.ms)],
+                    child: widget.calType == CalType.MONTHVIEW
+                        ? CalendarTile(
+                      title: ((index - widget.firstDaySpacing < 0
+                          ? "${getDaysInMonth(widget.currMonth == 1 ? (widget.currYear - 1) : (widget.currYear), (widget.currMonth - 1 < 1 ? 12 : widget.currMonth - 1)) + 1 - (widget.firstDaySpacing) + index}"
+                          : index >=
+                          (getDaysInMonth(widget.currYear,
+                              widget.currMonth) +
+                              widget.firstDaySpacing)
+                          ? "${index + 1 - (getDaysInMonth(widget.currYear, widget.currMonth) + widget.firstDaySpacing)}"
+                          : "${(index + 1) - widget.firstDaySpacing}"))
+                          .toString(),
+                      events: item,
+                      onClick: (events) {
+                        invokeOnclick(events);
+                      },
+                      isEnabled: (index - widget.firstDaySpacing < 0
+                          ? false
+                          : index >=
+                          (getDaysInMonth(widget.currYear,
+                              widget.currMonth) +
+                              widget.firstDaySpacing)
+                          ? false
+                          : true),
+                      date: (index - widget.firstDaySpacing < 0
+                          ? "" //previous month dates
+                          : index >=
+                          (getDaysInMonth(widget.currYear,
+                              widget.currMonth) +
+                              widget.firstDaySpacing)
+                          ? "" //next month dates
+                          : "${index - widget.firstDaySpacing + 1 < 10 ? "0${index - widget.firstDaySpacing + 1}" : index - widget.firstDaySpacing + 1}-${widget.currMonth < 10 ? "0${widget.currMonth}" : widget.currMonth}-${widget.currYear}"),
+                      todaysDate:
+                      widget.defaultFormat.format(DateTime.now()),
+                      tileBorderColor: widget.tileBorderColor,
+                      tileTextColor: widget.tileDateColor,
+                    )
+                        : CalendarDotTile(
+                      title: ((index - widget.firstDaySpacing < 0
+                          ? "${getDaysInMonth(widget.currMonth == 1 ? (widget.currYear - 1) : (widget.currYear), (widget.currMonth - 1 < 1 ? 12 : widget.currMonth - 1)) + 1 - (widget.firstDaySpacing) + index}"
+                          : index >=
+                          (getDaysInMonth(widget.currYear,
+                              widget.currMonth) +
+                              widget.firstDaySpacing)
+                          ? "${index + 1 - (getDaysInMonth(widget.currYear, widget.currMonth) + widget.firstDaySpacing)}"
+                          : "${(index + 1) - widget.firstDaySpacing}"))
+                          .toString(),
+                      events: item,
+                      onClick: (events) {
+                        invokeOnclick(events);
+                      },
+                      isEnabled: (index - widget.firstDaySpacing < 0
+                          ? false
+                          : index >=
+                          (getDaysInMonth(widget.currYear,
+                              widget.currMonth) +
+                              widget.firstDaySpacing)
+                          ? false
+                          : true),
+                      date: (index - widget.firstDaySpacing < 0
+                          ? "" //previous month dates
+                          : index >=
+                          (getDaysInMonth(widget.currYear,
+                              widget.currMonth) +
+                              widget.firstDaySpacing)
+                          ? "" //next month dates
+                          : "${index - widget.firstDaySpacing + 1 < 10 ? "0${index - widget.firstDaySpacing + 1}" : index - widget.firstDaySpacing + 1}-${widget.currMonth < 10 ? "0${widget.currMonth}" : widget.currMonth}-${widget.currYear}"),
+                      todaysDate:
+                      widget.defaultFormat.format(DateTime.now()),
+                      tileBorderColor: widget.tileBorderColor,
+                      tileTextColor: widget.tileDateColor,
+                    ),
+                  );
+                }),
+          ),
+        ),
+      ],
+    );
+  }
 }
